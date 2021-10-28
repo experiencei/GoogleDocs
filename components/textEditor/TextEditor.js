2aeabf474ea631ff530f5e55c017e178a3f39715
experiencei <ayelojahighbee01@gmail.com>
2021-10-28 22:50:23 +0100

    wotking on the text editor

--- components/textEditor/TextEditor.js
+++ components/textEditor/TextEditor.js
@@ -1,5 +1,10 @@
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css"
+import { Editor } from "react-draft-wysiwyg"
 
+const Editor = dynamic(
+    () => import('react-draft-wysiwyg').then(module => module.Editor), {
+    ssr: false,
+  })
 function TextEditor() {
     return (
         <div>
