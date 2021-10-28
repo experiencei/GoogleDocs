5a211c5f0d2784697509ea6ea08253b9f4efaf29
experiencei <ayelojahighbee01@gmail.com>
2021-10-28 17:36:48 +0100

    working on the document part

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -18,3 +18,21 @@ export default NextAuth({
   // // A database is optional, but required to persist accounts in a database
 //   database: process.env.DATABASE_URL,
 })
+
+
+
+// https://console.cloud.google.com/projectselector2/apis/credentials?supportedpurview=project
+
+
+// 1092371208251-3q0mncktsoi6pp5v0s4qbp0th09vkup9.apps.googleusercontent.com
+
+
+
+// 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
+
+
+
+// 261862721458-9opnk6h2t577afrg35lir18ndmqb2g5l.apps.googleusercontent.com
+
+
+// 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
