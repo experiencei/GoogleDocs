bc75046f5cfd3e26ea956d1a9d35435867552ba2
experiencei <ayelojahighbee01@gmail.com>
2021-10-28 22:43:09 +0100

    wotking on the text editor

--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -66,3 +66,14 @@ function Doc() {
 }
 
 export default Doc;
+
+
+export async function getServerSideProps(context) {
+    const session = await getSession(context);
+
+    return {
+        props : {
+            session,
+        },
+    };
+}
