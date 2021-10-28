1468cff0cc93d8df46150242a18df9143fbc6506
experiencei <ayelojahighbee01@gmail.com>
2021-10-28 20:57:47 +0100

    working on document row components

--- components/documentRow/DocumentRow.js
+++ components/documentRow/DocumentRow.js
@@ -6,7 +6,10 @@ import { useRouter } from "next/dist/client/router";
 function DocumentRow({ id , fileName, date}) {
     return (
         <div>
-            
+              <Icon
+              name="article"  size="3xl" color="blue"/>
+              <p className="flex-grow pl-5 w-10 pr-10 truncate">{fileName}</p>
+              <p className="pr-5 text-sm ">{date?.toDate().toLocaleDateString()}</p>
         </div>
     )
 }
