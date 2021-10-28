4c3def92c75a6763903f3c6d00e13f66bd228130
experiencei <ayelojahighbee01@gmail.com>
2021-10-28 17:48:18 +0100

    working on the ssr of server

--- pages/index.js
+++ pages/index.js
@@ -73,3 +73,17 @@ export default function Home() {
     </div>
   )
 }
+
+
+
+export async function getServerSideProps(context){
+   const session = await getSession(context);
+
+   return {
+     props : {
+       session,
+     }
+   }
+
+
+}
