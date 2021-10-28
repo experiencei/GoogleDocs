ea16de3faee876ae652b3a3b73cd571f520b35fa
experiencei <ayelojahighbee01@gmail.com>
2021-10-28 14:28:17 +0100

    initializing the tailwind css

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,11 @@ export default function Home() {
       </Head>
       
        <Header/>
-          <section>
+          <section  className="bg-[#F8F9FA] pb-10 px-10">
             <div className="max-w-3xl mx-auto">
-
+                <div className="py-6">
+                  <h2>Start a new document</h2>
+                </div>
             </div>
           </section>
     </div>
