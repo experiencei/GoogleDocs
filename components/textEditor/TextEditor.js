18de5bec1833ed5cdc251491235088d7e4a72ca5
experiencei <ayelojahighbee01@gmail.com>
2021-10-28 23:05:09 +0100

    wotking on the text editor

--- components/textEditor/TextEditor.js
+++ components/textEditor/TextEditor.js
@@ -11,11 +11,16 @@ const Editor = dynamic(
 function TextEditor() {
    const [editorState , setEditorState] = useState(EditorState.createEmpty())
 
+    const onEditorStateChange = () => {
 
+    };
+
+    
     return (
         <div className="bg-[#F8F9FA] min-h-screen pb-16">
             <Editor 
                editorState={editorState}
+               onEditorStateChange={onEditorStateChange}
                 toolbarClassName="flex sticky top-0 z-50 !justify-center mx-auto"
                 editorClassName="mt-6 p-10 bg-white shadow-lg max-w-5xl mx-auto mb-12 border"
             />
