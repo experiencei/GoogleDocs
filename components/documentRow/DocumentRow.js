1b4e2fe7d36bde74c851fb87236454f5d6f8be22
experiencei <ayelojahighbee01@gmail.com>
2021-10-28 21:55:14 +0100

    working on document row components

--- components/documentRow/DocumentRow.js
+++ components/documentRow/DocumentRow.js
@@ -8,7 +8,7 @@ function DocumentRow({ id , fileName, date}) {
 
 
     return (
-        <div onClick={() => router.push(`/docs/${id}`)} className="flex items-center p-4 rounded-lg hover:bg-gray-100 text-gray-700 text-sm cursor-pointer">
+        <div onClick={() => router.push(`/doc/${id}`)} className="flex items-center p-4 rounded-lg hover:bg-gray-100 text-gray-700 text-sm cursor-pointer">
               <Icon
               name="article"  size="3xl" color="blue"/>
               <p className="flex-grow pl-5 w-10 pr-10 truncate">{fileName}</p>
