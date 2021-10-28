82a03616e6fd17c9a6d1887578d29abfe8d5356a
experiencei <ayelojahighbee01@gmail.com>
2021-10-28 21:08:37 +0100

    working on document row components

--- components/documentRow/DocumentRow.js
+++ components/documentRow/DocumentRow.js
@@ -4,8 +4,11 @@ import { useRouter } from "next/dist/client/router";
 
 
 function DocumentRow({ id , fileName, date}) {
+    const router = useRouter();
+
+
     return (
-        <div className="flex items-center p-4 rounded-lg hover:bg-gray-100 text-gray-700 text-sm cursor-pointer">
+        <div onClick={() => router.push(`/docs/${id}`)} className="flex items-center p-4 rounded-lg hover:bg-gray-100 text-gray-700 text-sm cursor-pointer">
               <Icon
               name="article"  size="3xl" color="blue"/>
               <p className="flex-grow pl-5 w-10 pr-10 truncate">{fileName}</p>
