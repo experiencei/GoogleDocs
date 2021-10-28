6ee6cd40bc44e558b811d51c5d73f2eef750f1b1
experiencei <ayelojahighbee01@gmail.com>
2021-10-28 13:52:04 +0100

    initializing the tailwind css

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,7 +1,26 @@
+import Button from "@material-tailwind/react/Button"
+import Icon from "@material-tailwind/react/Icon"
+
+
 function Header() {
     return (
         <div className="flex items-center">
+            <Button
+              color="gray"
+              buttonType="outline"
+              rounded={true}
+              iconOnly={true}
+              ripple="dark"
+              className="hidden md:inline-flex h-20 w-20 border-0"
             
+            >
+                <Icon
+                    size="3xl"
+                    name="menu"
+                />
+            </Button>
+
+
         </div>
     )
 }
