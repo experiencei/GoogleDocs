5f98bb8f344a3f2880535d1b092054a33d28fb9d
experiencei <ayelojahighbee01@gmail.com>
2021-10-28 21:06:06 +0100

    working on document row components

--- components/documentRow/DocumentRow.js
+++ components/documentRow/DocumentRow.js
@@ -10,6 +10,21 @@ function DocumentRow({ id , fileName, date}) {
               name="article"  size="3xl" color="blue"/>
               <p className="flex-grow pl-5 w-10 pr-10 truncate">{fileName}</p>
               <p className="pr-5 text-sm ">{date?.toDate().toLocaleDateString()}</p>
+
+              <Button
+              color="gray"
+              buttonType="outline"
+              rounded={true}
+              iconOnly={true}
+              ripple="dark"
+              className="border-0"
+            
+            >
+                <Icon
+                    size="3xl"
+                    name="more_vert"
+                />
+            </Button>
         </div>
     )
 }
