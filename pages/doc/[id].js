42ed43080b0ce96be0dd8947530e9c155e8dd7f2
experiencei <ayelojahighbee01@gmail.com>
2021-10-28 21:47:24 +0100

    working on document row components

--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -2,13 +2,18 @@ import Button from "@material-tailwind/react/Button"
 import Icon from "@material-tailwind/react/Icon"
 import { useRouter } from "next/dist/client/router";
 import { useDocumentOnce } from "react-firebase-hooks/firestore";
-
+import db from "../../components/firebase/firebase"
 import { getSession , signOut , useSession } from "next-auth/client";
-
-
+import Login from "../../components/login/Login"
 
 
 function Doc() {
+  const [ session] = useSession();
+
+  if(!session) return <Login/>
+ 
+  
+
     return (
         <div>
             <Login/>
