3ef54711850508b8b77dc1800a7e3aa52f8406e2
experiencei <ayelojahighbee01@gmail.com>
2021-10-28 20:54:17 +0100

    working on document row components

--- components/documentRow/DocumentRow.js
+++ components/documentRow/DocumentRow.js
@@ -1,3 +1,8 @@
+import Button from "@material-tailwind/react/Button"
+import Icon from "@material-tailwind/react/Icon"
+import { useRouter } from "next/dist/client/router";
+
+
 function DocumentRow({ id , fileName, date}) {
     return (
         <div>
