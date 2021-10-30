2c0b39b76a4322ef763b7e6a645dbbf177c53c24
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 16:16:17 +0100

    hooks rule obeyed for ease deploying

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
 
   images: {
-    domains : ["www.google.com" , 'ng.linkedin.com' , "links.papareact.com"]
+    domains : ["www.google.com" , 'ng.linkedin.com' , "links.papareact.com" , "lh3.googleusercontent.com"]
   },
 }
